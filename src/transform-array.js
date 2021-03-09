23cb01c4aaa0f775191b5e514febe5171d064237
Baimenovbn <baibn@mail.ru>
2021-03-10 03:35:50 +0600

    feat: transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error()
+  }
+  let answer = []
+
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case '--discard-next':
+        i++
+        break
+      case '--discard-prev':
+        if (arr[i - 2] === '--discard-next') break;
+        answer.pop()
+        break
+      case '--double-next':
+        let next = arr[i + 1]
+        next !== undefined ? answer.push(next) : ''
+        break
+      case '--double-prev':
+        if (arr[i - 2] === '--discard-next') break;
+        let prev = arr[i - 1]
+        prev !== undefined ? answer.push(prev) : ''
+        break
+      default:
+        answer.push(arr[i]);
+    }
+  }
+
+  return answer
+  
 };
