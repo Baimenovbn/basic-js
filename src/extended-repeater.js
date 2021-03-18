f351525038550d869641cb70716f0ed6050793d2
Baimenovbn <baibn@mail.ru>
2021-03-18 11:22:38 +0600

    feat: add extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+
+module.exports = function repeater(str, {
+  repeatTimes = 1, 
+  separator = '+', 
+  addition = '',
+  additionRepeatTimes = 1,
+  additionSeparator = '|',
+} = {}) {
   
+  let added = []
+  let formattedAddition = String(addition);
+  let formattedString = String(str)
+  
+  for (let i = 1; i <= additionRepeatTimes; i++) {
+    added.push(formattedAddition)
+  }
+  
+  let repeated = []
+  for (let i = 1; i <= repeatTimes; i++) {
+    repeated.push(formattedString + added.join(additionSeparator))
+  }
+
+
+  return repeated.join(separator)
+};
